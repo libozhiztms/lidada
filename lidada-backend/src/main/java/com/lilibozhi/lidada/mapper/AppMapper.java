b1fe4429efa4f8c4b1f14bf4736b412246e5ffb8
lilibozhi <3076474518@qq.com>
2024-08-12 07:14:18 +0800

    后端开发

---
+++ lidada-backend/src/main/java/com/lilibozhi/lidada/mapper/AppMapper.java
@@ -0,0 +1,18 @@
+package com.lilibozhi.lidada.mapper;
+
+import com.lilibozhi.lidada.model.entity.App;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lilibozhi
+* @description 针对表【app(应用)】的数据库操作Mapper
+* @createDate 2024-08-10 09:55:24
+* @Entity com.lilibozhi.lidada.model.entity.App
+*/
+public interface AppMapper extends BaseMapper<App> {
+
+}
+
+
+
+
---
+++ lidada-backend/src/main/java/com/lilibozhi/lidada/mapper/ScoringResultMapper.java
@@ -0,0 +1,18 @@
+package com.lilibozhi.lidada.mapper;
+
+import com.lilibozhi.lidada.model.entity.ScoringResult;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lilibozhi
+* @description 针对表【scoring_result(评分结果)】的数据库操作Mapper
+* @createDate 2024-08-10 09:55:24
+* @Entity com.lilibozhi.lidada.model.entity.ScoringResult
+*/
+public interface ScoringResultMapper extends BaseMapper<ScoringResult> {
+
+}
+
+
+
+
